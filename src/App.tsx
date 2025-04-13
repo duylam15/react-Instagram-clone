8a37ba8b83f089b922281c28bf060e4008aca732
Duy Lam <duylam468213@gmail.com>
2025-04-13 11:46:41 +0700

    fix loi 2 giao dien

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { RouterProvider, Route, Routes, HashRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { CSpinner } from "@coreui/react";
 import '@coreui/coreui/dist/css/coreui.min.css';
@@ -14,41 +14,30 @@ import { router } from "./routes";
 import store from "./store";
 import { RefreshProvider } from "./contexts/RefreshContext";
 import { NotificationSocketProvider } from "./contexts/NotificationSocketContext";
-// Lazy load layout của admin
-const DefaultLayoutAdmin = React.lazy(() => import("./layout/DefaultLayoutAdmin"));
-const App: React.FC = () => {
 
+const App: React.FC = () => {
   return (
     <NotificationSocketProvider
       userId={66}
       onReceive={(notify) => console.log("🔔 " + notify.content)}
     >
-      {/* App nội dung ở đây */}
-
       <RefreshProvider>
         <ThemeProvider>
-          <RouterProvider router={router} />
-          <HashRouter>
-            <Suspense
-              fallback={
-                <div className="pt-3 text-center">
-                  <CSpinner color="primary" variant="grow" />
-                </div>
-              }
-            >
-              <Routes>
-                <Route path="admin/*" element={<DefaultLayoutAdmin />} />
-              </Routes>
-            </Suspense>
-          </HashRouter>
+          <Suspense
+            fallback={
+              <div className="pt-3 text-center">
+                <CSpinner color="primary" variant="grow" />
+              </div>
+            }
+          >
+            <RouterProvider router={router} />
+          </Suspense>
         </ThemeProvider>
       </RefreshProvider>
     </NotificationSocketProvider>
-
   );
 };
 
-// Render App vào DOM
 createRoot(document.getElementById("root") as HTMLElement).render(
   <Provider store={store}>
     <App />
--- src/pages/error/index.tsx
+++ src/pages/error/index.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Error() {
 	return (
-		<div>Error</div>
+		<div className="text-center py-20">
+			<h1 className="text-3xl font-bold mb-4 text-red-500">Có lỗi xảy ra 😢</h1>
+			<p className="mb-4">Trang bạn đang tìm kiếm không thể hiển thị hoặc đã xảy ra lỗi không mong muốn.</p>
+			<a href="/" className="text-blue-500 underline">Quay lại trang chủ</a>
+		</div>
 	)
 }
